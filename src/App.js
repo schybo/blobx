2aa15f8ce4d7cc66dff7c3e87599ac7059573e14
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2019-12-08 16:51:55 -0500

    Got charts working with recharts

--- src/App.js
+++ src/App.js
@@ -3,62 +3,26 @@ import PropTypes from "prop-types";
 import MaskedInput from "react-text-mask";
 import NumberFormat from "react-number-format";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
-import FilledInput from "@material-ui/core/FilledInput";
-import FormControl from "@material-ui/core/FormControl";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-import MyChart from './MyChart.js'
+import MyChart from "./MyChart.js";
 
-const useStyles = makeStyles((theme) =>
+const useStyles = makeStyles(theme =>
   createStyles({
     container: {
       display: "flex",
-      flexWrap: "wrap"
+      flexWrap: "wrap",
+      paddingTop: "30px"
     },
     formControl: {
       margin: theme.spacing(1)
     }
   })
 );
-
-function TextMaskCustom(props) {
-  const { inputRef, ...other } = props;
-
-  return (
-    <MaskedInput
-      {...other}
-      ref={ref => {
-        inputRef(ref ? ref.inputElement : null);
-      }}
-      mask={[
-        "(",
-        /[1-9]/,
-        /\d/,
-        /\d/,
-        ")",
-        " ",
-        /\d/,
-        /\d/,
-        /\d/,
-        "-",
-        /\d/,
-        /\d/,
-        /\d/,
-        /\d/
-      ]}
-      placeholderChar={"\u2000"}
-      showMask
-    />
-  );
-}
-
-TextMaskCustom.propTypes = {
-  inputRef: PropTypes.func.isRequired
-};
 
 function NumberFormatCustom(props) {
   const { inputRef, onChange, id, ...other } = props;
@@ -88,119 +52,105 @@ NumberFormatCustom.propTypes = {
 };
 
 export default function ComposedTextField() {
-  const [labelWidth, setLabelWidth] = React.useState(0);
-  const [name, setName] = React.useState("Composed TextField");
-  const labelRef = React.useRef(null);
-  const inputRef = React.useRef(null);
   const classes = useStyles();
 
   const [values, setValues] = React.useState({
-    textmask: "(1  )    -    ",
-    income: "1320",
-    savings: "700"
+    // textmask: "(1  )    -    ",
+    income: "6600",
+    savings: "3000",
+    rent: "1350",
+    months: 12
   });
 
   const handleChange = name => event => {
-    console.log(name);
-    console.log(event);
     setValues({
       ...values,
       [name]: event.target.value
     });
   };
 
-  React.useEffect(() => {
-    setLabelWidth(labelRef.current.offsetWidth);
-  }, []);
-
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setName(event.target.value);
-  // };
-
   return (
     <div className={classes.container}>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="formatted-text-mask-input">
-          react-text-mask
-        </InputLabel>
-        <Input
-          value={values.textmask}
-          onChange={handleChange("textmask")}
-          id="formatted-text-mask-input"
-          inputComponent={TextMaskCustom}
-        />
-      </FormControl>
-      <TextField
-        className={classes.formControl}
-        label="react-number-format"
-        value={values.income}
-        onChange={handleChange("income")}
-        id="formatted-income-input"
-        InputProps={{
-          inputComponent: NumberFormatCustom
-        }}
-      />
-      <TextField
-        className={classes.formControl}
-        label="react-number-format"
-        value={values.savings}
-        onChange={handleChange("savings")}
-        id="formatted-savings-input-2"
-        InputProps={{
-          inputComponent: NumberFormatCustom
-        }}
-      />
-      <MyChart monthlySavings={values.savings} months={12}></MyChart>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="component-simple">Name</InputLabel>
-        <Input id="component-simple" value={name} onChange={handleChange} />
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="component-helper">Name</InputLabel>
-        <Input
-          id="component-helper"
-          value={name}
-          onChange={handleChange}
-          aria-describedby="component-helper-text"
-        />
-        <FormHelperText id="component-helper-text">
-          Some important helper text
-        </FormHelperText>
-      </FormControl>
-      <FormControl className={classes.formControl} disabled>
-        <InputLabel htmlFor="component-disabled">Name</InputLabel>
-        <Input id="component-disabled" value={name} onChange={handleChange} />
-        <FormHelperText>Disabled</FormHelperText>
-      </FormControl>
-      <FormControl className={classes.formControl} error>
-        <InputLabel htmlFor="component-error">Name</InputLabel>
-        <Input
-          id="component-error"
-          value={name}
-          onChange={handleChange}
-          aria-describedby="component-error-text"
-        />
-        <FormHelperText id="component-error-text">Error</FormHelperText>
-      </FormControl>
-      <FormControl className={classes.formControl} variant="outlined">
-        <InputLabel ref={labelRef} htmlFor="component-outlined">
-          Name
-        </InputLabel>
-        <OutlinedInput
-          id="component-outlined"
-          value={name}
-          onChange={handleChange}
-          labelWidth={labelWidth}
-        />
-      </FormControl>
-      <FormControl className={classes.formControl} variant="filled">
-        <InputLabel htmlFor="component-filled">Name</InputLabel>
-        <FilledInput
-          id="component-filled"
-          value={name}
-          onChange={handleChange}
-        />
-      </FormControl>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h1" component="h1" align="center" gutterBottom>
+            Brent's Savings
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            display="flex"
+            p={1}
+            flexWrap="wrap"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <TextField
+              className={classes.formControl}
+              label="Monthly Income"
+              value={values.income}
+              onChange={handleChange("income")}
+              id="formatted-income-input"
+              InputProps={{
+                inputComponent: NumberFormatCustom
+              }}
+            />
+            <TextField
+              className={classes.formControl}
+              label="Savings"
+              value={values.savings}
+              onChange={handleChange("savings")}
+              id="formatted-savings-input"
+              InputProps={{
+                inputComponent: NumberFormatCustom
+              }}
+            />
+            <TextField
+              className={classes.formControl}
+              label="Rent"
+              value={values.rent}
+              onChange={handleChange("rent")}
+              id="formatted-rent-input"
+              InputProps={{
+                inputComponent: NumberFormatCustom
+              }}
+            />
+            <TextField
+              className={classes.formControl}
+              label="Months"
+              value={values.months}
+              onChange={handleChange("months")}
+              id="formatted-months-input"
+            />
+            {/* <FormControl className={classes.formControl} error>
+              <InputLabel htmlFor="component-error">Name</InputLabel>
+              <Input
+                id="component-error"
+                value={name}
+                onChange={handleChange}
+                aria-describedby="component-error-text"
+              />
+              <FormHelperText id="component-error-text">Error</FormHelperText>
+            </FormControl> */}
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            display="flex"
+            p={1}
+            flexWrap="wrap"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <MyChart
+              monthlyIncome={values.income}
+              monthlySavings={values.savings}
+              months={values.months}
+              rent={values.rent}
+            ></MyChart>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/MyChart.js
+++ src/MyChart.js
@@ -1,46 +1,65 @@
 import React from "react";
-import { Chart } from "react-charts";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+  ResponsiveContainer
+} from "recharts";
 
-const calculateSavingsPoints = (savings, months) => {
-  const dataPoints = []
+const calculateSavingsPoints = (income, savings, months, rent) => {
+  const dataPoints = [];
   for (let i = 0; i < months; i++) {
-    dataPoints.push([i, savings * (i+1)])
+    dataPoints.push({
+      name: `Month ${i + 1}`,
+      pv: savings * (i + 1),
+      uv: income * (i + 1),
+      vv: rent * (i + 1)
+    });
   }
-  return dataPoints
-}
+  return dataPoints;
+};
 
 function MyChart(props) {
-  const { monthlyIncome, monthlySavings, months } = props;
-  const data = React.useMemo(
-    () => [
-      {
-        label: "Brent's Savings",
-        data: calculateSavingsPoints(monthlySavings, months)
-      }
-    ],
-    // []
-    [monthlySavings, months]
-  );
-
-  const axes = React.useMemo(
-    () => [
-      { primary: true, type: "linear", position: "bottom" },
-      { type: "linear", position: "left" }
-    ],
-    []
+  const { monthlyIncome, monthlySavings, months, rent } = props;
+  // console.log("HERE");
+  // const data = React.useMemo(
+  //   () => [
+  //     {
+  //       data: calculateSavingsPoints(monthlySavings, months)
+  //     }
+  //   ],
+  //   // []
+  //   [monthlySavings, months]
+  // );
+  const data = calculateSavingsPoints(
+    monthlyIncome,
+    monthlySavings,
+    months,
+    rent
   );
 
   const lineChart = (
-    // A react-chart hyper-responsively and continuusly fills the available
+    // A react-chart hyper-responsively and continously fills the available
     // space of its parent element automatically
-    <div
-      style={{
-        width: "400px",
-        height: "300px"
-      }}
-    >
-      <Chart data={data} axes={axes} />
-    </div>
+    <ResponsiveContainer width="90%" height={250}>
+      <LineChart
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="vv" stroke="#858a9d" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 
   return lineChart;
